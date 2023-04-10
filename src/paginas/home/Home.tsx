862786cce5839a07473a658139246ee091bbdc6c
Lihsolive <liliamoliveira13@gmail.com>
2023-04-10 20:53:07 -0300

    Criando o componente Home

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <>
+      <h1 className="titulo">Home</h1>
+      <img className="img" src="src/assets/001.jpg" alt="Imagem tela inicial" />
+    </>
+  );
+}
+
+export default Home;
