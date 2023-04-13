522519d8658a8d7b83a91b146cf52ec22803480e
Lihsolive <liliamoliveira13@gmail.com>
2023-04-12 22:00:18 -0300

    adicionando os primeiros estilos a pagina

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -3,40 +3,41 @@ import FacebookIcon from "@material-ui/icons/Facebook";
 import InstagramIcon from "@material-ui/icons/Instagram";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import { Typography, Box, Grid } from "@material-ui/core";
+import "./Footer.css"
 
 function Footer() {
   return (
     <>
-      <Grid container direction="row" justifyContent="center" alignItems="center">
-        <Grid alignItems="center" item xs={12}>
-          <Box style={{ backgroundColor: "#3F51B5", height: "120px" }}>
-            <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-              <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }} > 
+      <Grid className="footer-container" container direction="row" justifyContent="center" alignItems="center">
+        <Grid className="footer-grid" alignItems="center" item xs={12}>
+          <Box className="footer-box">
+            <Box className="footer-title" padding={1} display="flex" alignItems="center" justifyContent="center">
+              <Typography className="footer-icons-title" variant="h5" align="center" gutterBottom > 
                 Siga-nos nas redes sociais{" "}
               </Typography>
             </Box>
-            <Box display="flex" alignItems="center" justifyContent="center">
-              <a href="https://www.facebook.com/generationbrasil" target="_blank">
-                <FacebookIcon style={{ fontSize: 60, color: "white" }} />
+            <Box className="footer-icons"  display="flex" alignItems="center" justifyContent="center">
+              <a className="footer-icons-item" href="https://www.facebook.com/generationbrasil" target="_blank">
+                <FacebookIcon style={{ fontSize: 30 }} />
               </a>
-              <a href="https://www.instagram.com/generationbrasil/" target="_blank" >
-                <InstagramIcon style={{ fontSize: 60, color: "white" }} />
+              <a className="footer-icons-item" href="https://www.instagram.com/generationbrasil/" target="_blank" >
+                <InstagramIcon style={{ fontSize: 30 }} />
               </a>
-              <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank" >
-                <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
+              <a className="footer-icons-item" href="https://www.linkedin.com/school/generationbrasil/" target="_blank" >
+                <LinkedInIcon style={{ fontSize: 30 }} />
               </a>
             </Box>
           </Box>
-          <Box style={{ backgroundColor: "#303F9F", height: "60px" }}>
+          <Box className="footer-final">
             <Box paddingTop={1}>
-              <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >
-                © 2020 Copyright:
+              <Typography className="footer-copyright" variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >
+              Copyright  © 2023  Liliam Oliveira. Todos os direitos reservados
               </Typography>
             </Box>
             <Box>
-              <a target="_blank" href="https://brasil.generation.org">
+              <a className="footer-link" target="_blank" href="https://github.com/Lihsolive">
                 <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">
-                  brasil.generation.org
+                github.com/Lihsolive
                 </Typography>
               </a>
             </Box>
