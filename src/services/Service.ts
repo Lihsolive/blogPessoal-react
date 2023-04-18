6138e48a9eeac67b7383733f0536b6fcb89b80ca
Lihsolive <liliamoliveira13@gmail.com>
2023-04-18 16:16:15 -0300

    Criando componetes de Postagens e Temas

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const api = axios.create({
+export const api = axios.create({ //create - cria um objeto de configuração através do axios. O axios libera todos os seus métodos disponíveis para uso
     baseURL: "https://blogpessoal-w6x8.onrender.com/"
 }) //está chamando a url será a api utilizada para realizar as requisições
 
@@ -10,14 +10,15 @@ export const cadastroUsuario = async(url:any, dados:any, setDado:any) => {
 }
 
 export const login = async(url:any, dados:any, setDado:any) => { //arrow function
-    const resposta = await api.post(url, dados) //será a resposta
-    setDado(resposta.data.token) 
+    const resposta = await api.post(url, dados) //faz a requisição e aguarda a resposta
+    setDado(resposta.data.token) //da resposta grava o token em algum lugar
 } 
-//any: significa que o tipo de informação pode ser qualquer um (string, number...)
+//any: significa que a informação pode ser de qualquer tipo (string, number...)
 //url: é o caminho do backend pra fazer o login (/usuarios/login)
 //dados: é um objeto que terá as informações necessárias para logar 
 // ("usuário": "string",
 // "senha": "string")
-//setDado(resposta.data.token) busca na resposta o token que tá dentro de data. Só retornará o token que houver usuário, do contrário dará erro
+//setDado(resposta.data.token) busca na resposta o token que tá dentro de data. Só retornará o token que houver usuário, do contrário dará erro.
+
 
 
